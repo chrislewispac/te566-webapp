e5a3ed40d868674e4412b6baa94b27dbeb1a992a
Chris Lewis <chrislewispac@users.noreply.github.com>
2020-12-19 19:52:34 -0800

    remove sensitive info, no longer have student github

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,7 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/firestore";
 
-//TODO: move to env file
-const fbConfig = {
-  apiKey: "AIzaSyDXUh0aHBgUeJnJtuk36mlvKJlWm3CrHSk",
-  authDomain: "te566-3775d.firebaseapp.com",
-  databaseURL: "https://te566-3775d.firebaseio.com",
-  projectId: "te566-3775d",
-  storageBucket: "te566-3775d.appspot.com",
-  messagingSenderId: "598528548691",
-  appId: "1:598528548691:web:3ff7a15c1ff0b94d5a0ba6",
-};
+const fbConfig = {};
 
 // Initialize firebase instance
 export const myFirebase = firebase.initializeApp(fbConfig);
